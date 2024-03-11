f19a94d0e09270436163987269018f50f0f0d7f7
Muhammad Ahmed Khan <ahmed.mak26@outlook.com>
2024-03-11 20:52:22 +0500

    Add files via upload

---
+++ scripts/pricing.js
@@ -0,0 +1,15 @@
+function showMenu(){
+
+  const menuBUTTON = document.querySelector('.sidebar');
+  menuBUTTON.style.display = 'flex';
+
+
+}
+
+function closeMenu(){
+
+  const menuBUTTON = document.querySelector('.sidebar');
+  menuBUTTON.style.display = 'none';
+
+}
+
